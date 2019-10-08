2c8ef85c251d085058c3fd999e69f47cb6eb3b6e
Adam Kulasiak <adam.kulasiak@gmail.com>
2019-10-08 19:36:42 +0200

    objects_zad0a

---
+++ 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
@@ -0,0 +1,17 @@
+// zad0a
+
+const city = {
+    capital: 'Warsaw',
+    population: 38000000,
+    president: 'Andrzej',
+    primeMinisters: [
+        'Marek ZAGÓRSKI',
+        'Dariusz PIONTKOWSKI',
+        'Krzysztof TCHÓRZEWSKI'
+    ]
+};
+
+console.log(`capital: ${city.capital}`);
+console.log(`population: ${city.population}`);
+console.log(`president: ${city.president}`);
+console.log(`primeMinisters: ${city.primeMinisters}`);
