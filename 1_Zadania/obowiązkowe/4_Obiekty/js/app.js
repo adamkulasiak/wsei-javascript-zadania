38aa3be5fc2c38d14f332637f4bef5eef2e1e3d0
Adam Kulasiak <adam.kulasiak@gmail.com>
2019-10-08 19:41:43 +0200

    objects_zad0b

--- 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
+++ 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
@@ -11,7 +11,20 @@ const city = {
     ]
 };
 
-console.log(`capital: ${city.capital}`);
-console.log(`population: ${city.population}`);
-console.log(`president: ${city.president}`);
-console.log(`primeMinisters: ${city.primeMinisters}`);
+// console.log(`capital: ${city.capital}`);
+// console.log(`population: ${city.population}`);
+// console.log(`president: ${city.president}`);
+// console.log(`primeMinisters: ${city.primeMinisters}`);
+
+//zad0b
+
+const timeMachine = {
+    shape: 'circle',
+    model: 'pumba',
+    run: (date, place) => {
+        console.log(`Now(${date}) we are in ${place}`);
+    }
+};
+console.log(`shape: ${timeMachine.shape}`);
+console.log(`model: ${timeMachine.model}`);
+timeMachine.run('2050-01-01', 'the Moon');
