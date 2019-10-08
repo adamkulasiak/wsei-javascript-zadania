671cbdad4d475ce12f7892b8e268b42c1c0bfe5f
adam.kulasiak <adam.kulasiak@gmail.com>
2019-10-08 17:08:41 +0200

    multidimensional_arrays_zad00

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -13,3 +13,27 @@ var task2Array = [
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+//zad00
+function checkArray(arr2D) {
+    let newArr = [];
+    arr2D.forEach(element => {
+        element.forEach(el => {
+            if(el %2 === 0) {
+                newArr.push(true);
+            }else {
+                newArr.push(false);
+            }
+        })   
+    });
+    console.log(newArr);
+    return newArr;
+}
+var arr = [
+    [11, 12],
+    [42, 2],
+    [-4, -120],
+    [0, 0],
+    [1, 34],
+    ];
+checkArray(arr);
