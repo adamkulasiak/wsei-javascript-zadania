9e392918f82218111c1b55698c03ae31c5684281
Adam Kulasiak <adam.kulasiak@gmail.com>
2019-10-10 18:14:52 +0200

    this_zad01

--- 1_Zadania/obowiązkowe/5_this/js/app.js
+++ 1_Zadania/obowiązkowe/5_this/js/app.js
@@ -12,6 +12,26 @@ const car = {
     }
 };
 
-console.log(car.printCarinfo());
-car.drive(20);
-console.log(car.printCarinfo());
+// console.log(car.printCarinfo());
+// car.drive(20);
+// console.log(car.printCarinfo());
+
+
+//zad01
+
+car.datesOfInspections = [
+    '2017-02-02',
+    '2018-02-02',
+    '2019-02-02'
+];
+
+car.addDateOfInspection = function(date) {
+    this.datesOfInspections.push(date);
+}
+
+car.getAllInspections = function() {
+    return this.datesOfInspections;
+}
+
+car.addDateOfInspection('2016-02-02');
+console.log(car.getAllInspections());
