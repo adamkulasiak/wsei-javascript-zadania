3b472a72de74cb89c67934c5ce52ceb635dbc8bd
Adam Kulasiak <adam.kulasiak@gmail.com>
2019-10-12 11:45:58 +0200

    dom_zad01

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -16,6 +16,16 @@ document.addEventListener("DOMContentLoaded", function () {
      // console.log(result);
 
 
+     //zad01
 
+     const home = document.querySelector('#home');
+     const home2 = document.getElementById('home');
+     console.log(home, home2);
+
+     const direction = document.querySelector('li');
+     console.log(direction);
+
+     const block = document.querySelector('.block');
+     console.log(block);
 
 });
