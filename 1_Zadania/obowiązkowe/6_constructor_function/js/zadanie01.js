78c3449bf8c5a6a1488e684ecff362a923507546
Adam Kulasiak <adam.kulasiak@gmail.com>
2019-10-12 09:09:08 +0200

    objects_zad01

--- 1_Zadania/obowiązkowe/6_constructor_function/js/zadanie01.js
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/zadanie01.js
@@ -1,23 +1,28 @@
-var Robot = function (name) {
+function Robot (name) {
     this.name = name;
     this.isFunctional = true;
 }
 
 Robot.prototype.sayHi = function (toWho) {
-    if (isFunctional === true) {
+    if (this.isFunctional === true) {
         console.log("Robot " + this.name + " greets " + toWho);
     }
     else {
-        console.log("Robot " + this.name + "is broken");
+        console.log("Robot " + this.name + " is broken");
     }
 };
 
 Robot.prototype.changeName = function (newname) {
-    console.log("Robot " + this.name + "changes name to " + newname);
-    name = newname;
+    console.log("Robot " + this.name + " changes name to " + newname);
+    this.name = newname;
 };
 
 Robot.prototype.fixIt = function () {
     this.isFunctional = true;
-    console.log("Robot " + this.name + "was fixed");
+    console.log("Robot " + this.name + " was fixed");
 };
+
+const robot = new Robot('Kazek');
+robot.sayHi('Maniek');
+robot.changeName('Wiesiek');
+robot.fixIt();
