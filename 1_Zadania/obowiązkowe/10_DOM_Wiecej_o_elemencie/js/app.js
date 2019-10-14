334d6d4701ccc6e809f8971b1917edae9ba6bb24
Adam Kulasiak <adam.kulasiak@gmail.com>
2019-10-14 19:10:47 +0200

    dom_moreElements_zad00

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -3,4 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+     //zad00
+
+    const ex5 = document.querySelector('.ex5 ul').children;
+    console.log(ex5);
+    for (let i = 0; i < ex5.length; i++) {
+        if (i === 0 || i % 2 === 0) {
+            ex5[i].style.backgroundColor = 'green';
+        }
+        if (i === 0 || i % 3 === 0) {
+            ex5[i].style.textDecoration = 'underline';
+        }
+        if (i === 4) {
+            ex5[i].classList.add('big');
+        }
+    }
 });
