7cad7f385a7d9e44f93b68c301c60ae0e088fc6d
adam.kulasiak <adam.kulasiak@gmail.com>
2019-12-22 22:35:50 +0100

    tooltips

--- 1_Zadania/dodatkowe/5_Tooltips/js/app.js
+++ 1_Zadania/dodatkowe/5_Tooltips/js/app.js
@@ -1,3 +1,24 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+
+ document.addEventListener('DOMContentLoaded', function() {
+    console.log('Dziala');
+
+    const tooltips = document.querySelectorAll('.tooltip');
+    console.log(tooltips);
+
+    tooltips.forEach(el => {
+        el.addEventListener('mouseover', function() {
+            console.log(this.dataset.text);
+            const span = document.createElement('span');
+            span.classList.add('tooltipText');
+            span.innerText = this.dataset.text;
+            this.appendChild(span);
+        });
+
+        el.addEventListener('mouseout', function() {
+            this.removeChild(this.children[0]);
+        });
+    });
+ });
