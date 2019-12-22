477cfaffad559feeb965c021e74176902361d97f
adam.kulasiak <adam.kulasiak@gmail.com>
2019-12-22 19:37:31 +0100

    slider

---
+++ 1_Zadania/dodatkowe/1_Slider/js/app.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const next = document.querySelector('#nextPicture');
+    const prev = document.querySelector('#prevPicture');
+    const list = document.querySelectorAll('.slider li');
+
+    console.log(next);
+    console.log(prev);
+    console.log(list);
+    let i = 0;
+    list[i].classList.add('visible');
+
+    next.addEventListener('click', function() {
+        list[i].classList.remove('visible');
+        i++;
+        if(i >= list.length) {
+            i = 0;
+        }
+        list[i].classList.add('visible');
+    });
+
+    prev.addEventListener('click', function() {
+        list[i].classList.remove('visible');
+        i--;
+        if(i < 0) {
+            i = list.length-1;
+        }
+        list[i].classList.add('visible'); 
+    });
+
+    //problem jest taki ze jak wyjde poza zakres tablicy to dostaje blad
+
+
+});
