3c6f1f1703fb1cbc41324562122148eff2821aeb
Adam Kulasiak <adam.kulasiak@gmail.com>
2019-12-22 15:20:38 +0100

    zad1 dom

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -5,8 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
      //zad00
 
-    const ex5 = document.querySelector('.ex5 ul').children;
-    console.log(ex5);
+    const ex5 = document.querySelectorAll('.ex5 li');
+    //console.log(ex5);
+    ex5[4].classList.add('big');
     for (let i = 0; i < ex5.length; i++) {
         if (i === 0 || i % 2 === 0) {
             ex5[i].style.backgroundColor = 'green';
@@ -14,8 +15,23 @@ document.addEventListener("DOMContentLoaded", function () {
         if (i === 0 || i % 3 === 0) {
             ex5[i].style.textDecoration = 'underline';
         }
-        if (i === 4) {
-            ex5[i].classList.add('big');
-        }
     }
+
+    //zad01
+
+    const chrome = document.querySelector('.chrome');
+    const edge = document.querySelector('.edge');
+    const firefox = document.querySelector('.firefox');
+
+    edge.style.backgroundImage = 'url("./assets/img/edge.png")';
+    firefox.style.backgroundImage = 'url("./assets/img/firefox.png")';
+
+    chrome.nextElementSibling.setAttribute('href', 'https://www.google.com/intl/pl_pl/chrome/');
+    chrome.nextElementSibling.innerText = 'Chrome';
+
+    edge.nextElementSibling.setAttribute('href', 'https://www.microsoft.com/pl-pl/windows/microsoft-edge');
+
+    firefox.nextElementSibling.setAttribute('href', 'https://www.mozilla.org/pl/firefox/new/');
+    firefox.nextElementSibling.innerText = 'Firefox';
+    chrome.style.width = '100px';
 });
