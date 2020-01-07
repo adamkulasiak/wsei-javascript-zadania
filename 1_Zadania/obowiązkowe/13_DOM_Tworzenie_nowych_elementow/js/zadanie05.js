271e230f1521dc86bdb375be8c67fece707e6b48
Adam Kulasiak <adam.kulasiak@gmail.com>
2020-01-07 17:09:46 +0100

    13_05

--- 1_Zadania/obowiązkowe/13_DOM_Tworzenie_nowych_elementow/js/zadanie05.js
+++ 1_Zadania/obowiązkowe/13_DOM_Tworzenie_nowych_elementow/js/zadanie05.js
@@ -1,26 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     
-    const list1btns = document.querySelectorAll('#list1 li .moveBtn');
+    let btns = document.querySelectorAll('.moveBtn');
     const list1 = document.querySelector('#list1');
-    const list2btns = document.querySelectorAll('#list2 li .moveBtn');
     const list2 = document.querySelector('#list2');
 
     console.log(list1);
 
-    list1btns.forEach(btn => {
+    btns.forEach(btn => {
         btn.addEventListener('click', function() {
             let li = this.parentNode;
-            li.parentNode.removeChild(li);
-            list2.appendChild(li);
+            if (li.parentNode == list1) {
+                list1.removeChild(li);
+                list2.appendChild(li);
+            } else {
+                list2.removeChild(li);
+                list1.appendChild(li);
+            }
         });
     });
 
-    list2btns.forEach(btn => {
-        btn.addEventListener('click', function() {
-            let li = this.parentNode;
-            li.parentNode.removeChild(li);
-            list1.appendChild(li);
-        });
-    });
 
 });
