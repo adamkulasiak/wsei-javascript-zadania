d19a8eeda2e22f9edff21808ddff5f3b8b42cc70
adam.kulasiak <adam.kulasiak@gmail.com>
2020-01-09 22:39:27 +0100

    11_4

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie04.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const btns = document.querySelectorAll('button');
+    const counter = document.querySelector('.counter');
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', function() {
+            let old = counter.innerHTML;
+            old++;
+            counter.innerHTML = old;
+        });
+    });
+
+});
